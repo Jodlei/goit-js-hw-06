3277049cd4069653d052f9e5132fbc6c0a3af45a
Yurii PErekrestnyi <jodlei1618@gmail.com>
2022-09-18 23:12:54 +0300

    10 завдання - на перевірку

--- js/task-10.js
+++ js/task-10.js
@@ -3,12 +3,32 @@ function getRandomHexColor() {
 }
 const createBtn = document.querySelector("[data-create]");
 const destroyBtn = document.querySelector("[data-destroy]");
-let size = 30;
+const input = document.querySelector("input");
+const parentDiv = document.querySelector("#boxes");
+const items = [];
+let amount = 0;
 function createBoxes(amount) {
-  for (let i = 0; i <= amount; i += 1) {
+  for (let i = 0, size = 30; i < amount; i += 1, size += 10) {
     const div = document.createElement("div");
     div.style.backgroundColor = getRandomHexColor();
-    div.style.width = "30px";
-    div.style.height = "30px";
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    items.push(div);
   }
+  return parentDiv.append(...items);
 }
+function givesAmountOfNumbers() {
+  amount = +input.value;
+  // return amount;
+}
+function createElements() {
+  createBoxes(amount);
+}
+function deleteAllElements() {
+  parentDiv.innerHTML = "";
+  input.value = "";
+  amount = 0;
+}
+input.addEventListener("input", givesAmountOfNumbers);
+createBtn.addEventListener("click", createElements);
+destroyBtn.addEventListener("click", deleteAllElements);
